a7cd864ddded44065e33c4d72e84d942c94c6ebf
muchirasamwel <muchirasamwel@gmail.com>
2025-02-20 16:47:55 +0300

    * Update the ignition module to match the campaign contract

--- ignition/modules/Campaign.ts
+++ ignition/modules/Campaign.ts
@@ -3,18 +3,10 @@
 
 import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
 
-const JAN_1ST_2030 = 1893456000;
-const ONE_GWEI: bigint = 1_000_000_000n;
+const CampaignModule = buildModule("CampaignModule", (m) => {
+  const campaign = m.contract("Campaign");
 
-const LockModule = buildModule("LockModule", (m) => {
-  const unlockTime = m.getParameter("unlockTime", JAN_1ST_2030);
-  const lockedAmount = m.getParameter("lockedAmount", ONE_GWEI);
-
-  const lock = m.contract("Campaign", [unlockTime], {
-    value: lockedAmount,
-  });
-
-  return { lock };
+  return { campaign };
 });
 
-export default LockModule;
+export default CampaignModule;
